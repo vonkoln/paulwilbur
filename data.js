99244acc89481fa44476216d4dcc45f1fd884a67
Bruno Stefano de Souza <40743118+vonkoln@users.noreply.github.com>
2024-01-01 00:12:03 -0300

    Update data.js

--- data.js
+++ data.js
@@ -4,6 +4,81 @@ export default  [
     artist: "",
     cover: "1.jpeg",
     file: "1.mp3",
+  },{
+    title:"1 -",
+    artist: "",
+    cover: "1.jpeg",
+    file: "2.mp3",
+  },{
+    title:"1 -",
+    artist: "",
+    cover: "1.jpeg",
+    file: "3.mp3",
+  },{
+    title:"1 -",
+    artist: "",
+    cover: "1.jpeg",
+    file: "4.mp3",
+  },{
+    title:"1 -",
+    artist: "",
+    cover: "1.jpeg",
+    file: "5.mp3",
+  },{
+    title:"1 -",
+    artist: "",
+    cover: "1.jpeg",
+    file: "6.mp3",
+  },{
+    title:"1 -",
+    artist: "",
+    cover: "1.jpeg",
+    file: "7.mp3",
+  },{
+    title:"1 -",
+    artist: "",
+    cover: "1.jpeg",
+    file: "8.mp3",
+  },{
+    title:"1 -",
+    artist: "",
+    cover: "1.jpeg",
+    file: "9.mp3",
+  },{
+    title:"1 -",
+    artist: "",
+    cover: "1.jpeg",
+    file: "10.mp3",
+  },{
+    title:"1 -",
+    artist: "",
+    cover: "1.jpeg",
+    file: "11.mp3",
+  },{
+    title:"1 -",
+    artist: "",
+    cover: "1.jpeg",
+    file: "12.mp3",
+  },{
+    title:"1 -",
+    artist: "",
+    cover: "1.jpeg",
+    file: "13.mp3",
+  },{
+    title:"1 -",
+    artist: "",
+    cover: "1.jpeg",
+    file: "14.mp3",
+  },{
+    title:"1 -",
+    artist: "",
+    cover: "1.jpeg",
+    file: "15.mp3",
+  },{
+    title:"1 -",
+    artist: "",
+    cover: "1.jpeg",
+    file: "16.mp3",
   }
  ];
 
